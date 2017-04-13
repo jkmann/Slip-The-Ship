61d7f201e156125e8203fd36344ecfc8455ec7c1
DLagos <dionmattlagos@gmail.com>
2017-04-12 20:46:17 -0700

    Initial setup

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaY = Input.GetAxis ("Vertical") * speed;
+		Vector3 newPosition = transform.position;
+		newPosition.x += deltaX * Time.deltaTime;
+		newPosition.z += deltaY * Time.deltaTime;
+		transform.position = newPosition;
+	}
+}
