01ac915a4898b95512e69fb50f1eea9cd4e35772
DLagos <dionmattlagos@gmail.com>
2017-04-29 15:32:15 -0700

    Fixed minimap camera and finished swapping aliens

---
+++ Assets/Scripts/Camera/MiniMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour {
+	public GameObject player;       
+	public float distance;
+	private Vector3 offset;  
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3 (transform.position.x, transform.position.y + distance, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
