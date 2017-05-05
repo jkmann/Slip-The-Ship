f086b59d0b8a6306259f22c1e52f74ab5a117e61
DLagos <dionmattlagos@gmail.com>
2017-05-04 20:03:02 -0700

    Added level 6

--- Assets/Supercyan Character Pack Free Sample/Scripts/SimpleCharacterControl.cs
+++ Assets/Supercyan Character Pack Free Sample/Scripts/SimpleCharacterControl.cs
@@ -178,7 +178,7 @@ public class SimpleCharacterControl : MonoBehaviour {
     {
         bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
 
-        if (jumpCooldownOver && m_isGrounded && Input.GetKey(KeyCode.Space))
+		if (jumpCooldownOver && m_isGrounded && CrossPlatformInputManager.GetButton("Jump"))
         {
             m_jumpTimeStamp = Time.time;
             m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
