2bbe1316489c272cdcefba6630f7024a9d958676
DLagos <dionmattlagos@gmail.com>
2017-05-12 21:36:23 -0700

    quick menu button bug fix

--- Assets/Scripts/Interface/GameManager.cs
+++ Assets/Scripts/Interface/GameManager.cs
@@ -41,7 +41,8 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		currentLevel.text = "Level: " + PlayerPrefs.GetInt ("CurLevel");
+
 
 	}
 
